844d038718dc9dd7a80e581e78e2c89483762b3d
dylan.k <bee2side@gmail.com>
2020-08-03 08:25:01 +0900

    set context

--- IOSflipCardAnimation_0801/UnitCardViewController.swift
+++ IOSflipCardAnimation_0801/UnitCardViewController.swift
@@ -8,7 +8,60 @@
 
 import UIKit
 
+
+
 class UnitCardViewController: UIViewController {
+    @IBOutlet weak var idCard: UIImageView!
+
+    var isOn = true
+    let idCardFrontImage = UIImage(named: "unitCard_front")
+    let idCardBackImage = UIImage(named: "unitCard_back")
+    let iconCerification = UIImage(named: "ico_certification")
+    var transform = CATransform3DIdentity
+    // flip animation
+    func cardFlipAnimation(delayNum: Double) {
+       // make image
+       let size = CGSize(width: idCardBackImage!.size.width, height: idCardBackImage!.size.height)
+       UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
+       idCardBackImage!.draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+       iconCerification!.draw(in: CGRect(x: 13, y: 13, width: 28, height: 28))
+       let newImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+       UIGraphicsEndImageContext()
+       // card animation
+       transform.m34 = 1.0 / 500.0
+       UIView.animate(withDuration: 0.25, delay: delayNum, options: .curveLinear, animations: {
+           self.transform = CATransform3DRotate(self.transform, CGFloat(90 * Double.pi / 180), 0, 1, 0)
+           self.idCard.layer.transform = self.transform
+           if self.isOn {
+               self.idCard.image = self.idCardFrontImage
+           } else {
+               self.idCard.image = newImage.withHorizontallyFlippedOrientation()
+           }
+       }, completion: { (_) in
+           UIView.animate(withDuration: 0.25, delay: 0, options: .curveLinear, animations: {
+               self.transform = CATransform3DRotate(self.transform, CGFloat(100 * Double.pi / 180), 0, 1, 0)
+               self.idCard.layer.transform = self.transform
+               
+               if self.isOn {
+                   self.idCard.image = newImage.withHorizontallyFlippedOrientation()
+                   self.isOn = false
+               } else {
+                   self.idCard.image = self.idCardFrontImage
+                   self.isOn = true
+               }
+           }, completion: { (_) in
+               UIView.animate(withDuration: 0.2, delay: 0, options: .curveLinear, animations: {
+                   self.transform = CATransform3DRotate(self.transform, CGFloat(-10 * Double.pi / 180), 0, 1, 0)
+                   self.idCard.layer.transform = self.transform
+               })
+           })
+       })
+    }
+    //tap gesture
+    @objc func touchToPickPhoto(_ gesture: UITapGestureRecognizer) {
+        cardFlipAnimation(delayNum: 0)
+    }
+
     
     //navigation
     let yourBackImage = UIImage(named: "wallet_commonBtnBack")
@@ -63,14 +116,18 @@ class UnitCardViewController: UIViewController {
         securityLabel0.transform = CGAffineTransform(translationX: securityLabelxPos, y: 0)
         securityLabel1.transform = CGAffineTransform(translationX: securityLabelxPos, y: 0)
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        cardFlipAnimation(delayNum: 0.5)
+        
+        // tap gesture
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(touchToPickPhoto(_:)))
+        idCard.addGestureRecognizer(tapGesture)
+        idCard.isUserInteractionEnabled = true
         
         navigationSet()
         backButtonCheck()
         securityTimer()
-        // Do any additional setup after loading the view.
     }
-
 }
--- IOSflipCardAnimation_0801/ViewController.swift
+++ IOSflipCardAnimation_0801/ViewController.swift
@@ -12,8 +12,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var kakaoCertificationBar: UIButton!
     @IBOutlet weak var certificationBar: UIButton!
     
-
-    
     let iconKakaoCerification = UIImage(named: "ico_certification_b")
     let iconCerification = UIImage(named: "ico_certification")
     let kakaoconLogo = UIImage(named: "commonIcoTitle")
@@ -27,7 +25,6 @@ class ViewController: UIViewController {
         navigationController?.navigationBar.topItem!.title = ""
         navigationController?.navigationBar.tintColor = UIColor(red: 25/255, green: 25/255, blue: 25/255, alpha: 1)
         navigationItem.titleView = UIImageView(image: kakaoconLogo)
-        
         // kakao ID card
         kakaoCertificationBar.setImage(iconKakaoCerification, for: .normal)
         kakaoCertificationBar.setTitle("Kakao ID Card", for: .normal)
@@ -38,7 +35,6 @@ class ViewController: UIViewController {
         kakaoCertificationBar.titleEdgeInsets = UIEdgeInsets(top: 0, left: 17, bottom: 0, right: 0)
         kakaoCertificationBar.layer.cornerRadius = 10
         kakaoCertificationBar.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
-        
         // kakao Unit card
         certificationBar.setImage(iconCerification, for: .normal)
         certificationBar.setTitle("Kakao Unit Card", for: .normal)
@@ -49,9 +45,17 @@ class ViewController: UIViewController {
         certificationBar.titleEdgeInsets = UIEdgeInsets(top: 0, left: 17, bottom: 0, right: 0)
         certificationBar.layer.cornerRadius = 10
         certificationBar.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
-        
     }
-    
-
 }
 
+class UIRoundPrimaryButton: UIButton{
+    required init(coder aDecoder: NSCoder){
+        super.init(coder: aDecoder)!
+        self.layer.cornerRadius = 15.0;
+        self.layer.borderWidth = 1
+        self.layer.borderColor = UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 0.14).cgColor
+        self.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1)
+        self.tintColor = UIColor(red: 25/255, green: 25/255, blue: 25/255, alpha: 1)
+    }
+    
+}
